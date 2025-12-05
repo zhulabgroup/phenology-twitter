ee28b201b196f28350fed871e22c4d07404af446
yiluansong <yiluansong@gmail.com>
2025-12-04 22:13:53 -0500

    test and debug

--- R/test_user_proportion.R
+++ R/test_user_proportion.R
@@ -1,5 +1,5 @@
 #' @export
-test_user_proportion <- function(end = NULL) {
+test_user_proportion <- function(ls_df_group_flow, ls_df_group_user_type, end = NULL) {
   ls_df_user_proportion <- vector(mode = "list")
   for (group in misc_subset(forlabel = F)) {
     ls_df_user_proportion[[group]] <- tidy_user_proportion(
@@ -23,10 +23,10 @@ test_user_proportion <- function(end = NULL) {
 #' @export
 tidy_user_proportion <- function(df_group_flow, df_group_user_type, end = NULL) {
   df <- df_group_flow %>%
-    select(user = !!sym(end), count) %>%
+    select(user = !!sym(end)) %>%
     left_join(df_group_user_type, by = "user") %>%
     group_by(type) %>%
-    summarise(count = sum(count)) %>%
+    summarise(count = n_distinct(user)) %>%
     drop_na()
 
   return(df)
